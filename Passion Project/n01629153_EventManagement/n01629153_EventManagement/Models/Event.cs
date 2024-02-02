64ccd071e79b20c2be8026cae1aad9211ceeaf67
Suyash0028 <suyashkulkarni43@gmail.com>
2024-02-01 23:36:51 -0500

    Updated model and created event data controller

--- Passion Project/n01629153_EventManagement/n01629153_EventManagement/Models/Event.cs
+++ Passion Project/n01629153_EventManagement/n01629153_EventManagement/Models/Event.cs
@@ -13,11 +13,26 @@ namespace n01629153_EventManagement.Models
         [Key]
         public int EventId { get; set; }
         public string EventName { get; set; }
-        public string EventDescription { get; set; } = string.Empty;
+        public string EventDescription { get; set; }
         public DateTime EventDate { get; set; }
         public string EventLocation { get; set; }
         [ForeignKey("EventUsers")]
         public int UserId { get; set; }
         public virtual User EventUsers { get; set; }
+
+        [ForeignKey("Sponsors")]
+        public int SponsorId { get; set; }
+        public virtual User SponsorName { get; set; }
+    }
+    public class EventDto
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime EventDate { get; set; }
+
+        public int SponsorName { get; set; }
+
+        public string UserName { get; set; }
     }
 }
